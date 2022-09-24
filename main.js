c94c92fb82dfdbdf05986c2175609d8b04e74eb9
YellowPool <101473363+YellowPool@users.noreply.github.com>
2022-09-24 11:29:52 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function the()
+{
+    window.location="the.html";
+}
+
+function fox()
+{
+    window.location="fox.html";
+}
+
+function sang()
+{
+    window.location="sang.html";
+}
+
+function a()
+{
+    window.location="a.html";
+}
+
+function song()
+{
+    window.location="song.html";
+}
+
+function back()
+{
+    window.location="index.html";
+}
